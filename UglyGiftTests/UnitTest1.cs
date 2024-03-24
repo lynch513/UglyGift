451f71a79b7aacfda3f2a6308d96ad4b8a1d3c7f
lynch513 <lynch513@yandex.ru>
2024-03-24 21:46:15 +0500

    wip

--- UglyGiftTests/UnitTest1.cs
+++ UglyGiftTests/UnitTest1.cs
@@ -5,22 +5,6 @@ namespace UglyGiftTests;
 
 public class Tests
 {
-    private string bigPrettyString = default!;
-
-    [OneTimeSetUp]
-    public void Init()
-    {
-        var assemblyDirectory = TestContext.CurrentContext.TestDirectory;
-        bigPrettyString = File.ReadAllText(Path.Combine(assemblyDirectory, "TestData", "tenthousand.txt"));
-        Trace.Listeners.Add(new ConsoleTraceListener());
-    }
-
-    [OneTimeTearDown]
-    public void EndTests()
-    {
-        Trace.Flush();
-    }
-
     [TestCaseSource(nameof(TestCasesForLineChunks))]
     public void IsPrettyLine_Should_Work(string input, IEnumerable<int> expectedArr, bool expectedStatus, int expectedChanges)
     {
@@ -42,21 +26,10 @@ public class Tests
         yield return new TestCaseData("110011", new [] {2, 2, 2}, true, 0);
         yield return new TestCaseData("00000000", new [] {8}, true, 0);
         // Ugly lines
-        yield return new TestCaseData("000101", new [] {3, 1, 1, 1}, false, 1); // 1
-        yield return new TestCaseData("00110100", new [] {2, 2, 1, 1, 2}, false, 3); // 3
-        yield return new TestCaseData("010111", new [] {1, 1, 1, 3}, false, 1); // 1
-        yield return new TestCaseData("110111", new [] {2, 1, 3}, false, 1); // 1
-        yield return new TestCaseData("110110", new [] {2, 1, 2, 1}, false, 2); // 2
-    }
-
-    [Test]
-    public void IsPrettyLine_Load_Test()
-    {
-        var stopWatch = Stopwatch.StartNew();
-        var chunks = PrettyStringParser.CountEqualChunks(bigPrettyString).ToList();
-        var isPrettyLine = PrettyStringParser.IsPrettyLine(chunks);
-        stopWatch.Stop();
-        TestContext.WriteLine($"Elapsed time: {stopWatch.ElapsedMilliseconds}");
-        isPrettyLine.Should().BeTrue();
+        yield return new TestCaseData("000101", new [] {3, 1, 1, 1}, false, 1);
+        yield return new TestCaseData("00110100", new [] {2, 2, 1, 1, 2}, false, 3);
+        yield return new TestCaseData("010111", new [] {1, 1, 1, 3}, false, 1);
+        yield return new TestCaseData("110111", new [] {2, 1, 3}, false, 1); /
+        yield return new TestCaseData("110110", new [] {2, 1, 2, 1}, false, 2);
     }
 }
