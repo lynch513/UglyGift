992ab096c004661095ec76c6cdbc112fd4cda25d
lynch513 <lynch513@yandex.ru>
2024-03-29 15:52:31 +0500

    wip

--- UglyGift/Program.cs
+++ UglyGift/Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace UglyGift {
     public static class PrettyStringParser {
-        public static IEnumerable<int> CountEqualChunks(IEnumerable<char> seq)
+        public static IEnumerable<(int Count, char Char)> CountEqualChunks(IEnumerable<char> seq)
         {
             var counter = 0;
             var ch = '0';
@@ -12,23 +12,27 @@ namespace UglyGift {
             {
                 if (i.index != 0 && i.value != ch)
                 {
-                    yield return counter;
+                    yield return (counter, ch);
                     counter = 0;
                 }
                 counter++;
                 ch = i.value;
             }
-            yield return counter;
+            yield return (counter, ch);
         }
 
-        public static bool IsPrettyLine(IList<int> chunks) => chunks.Distinct().Count() == 1;
+        public static bool IsPrettyLine(IList<(int Count, char Char)> chunks) => chunks.Select(i => i.Count).Distinct().Count() == 1;
 
-        public static int HowChangeNeeded(IList<int> chunks)
+        public static int HowChangeNeeded(IList<(int Count, char Char)> chunks)
         {
             if (IsPrettyLine(chunks))
                 return 0;
-            var max = chunks.Max();
-            return chunks.Count(i => i == max);
+            var maxChunk = chunks.MaxBy(i => i.Count);
+            var countMax = chunks.Count(i => i == maxChunk);
+            var chunksWithoutMax = chunks.Where(i => i != maxChunk).ToList();
+            var countRemainingZeros = chunksWithoutMax.Where(i => i.Char == '0').Sum(i => i.Count);
+            var countRemainingOnes = chunksWithoutMax.Where(i => i.Char == '1').Sum(i => i.Count);
+            return countMax % 2 == 0 ? Math.Max(countRemainingOnes, countRemainingZeros) : Math.Min(countRemainingOnes, countRemainingZeros);
         }
     }
 
--- UglyGiftTests/UnitTest1.cs
+++ UglyGiftTests/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace UglyGiftTests;
 public class Tests
 {
     [TestCaseSource(nameof(TestCasesForLineChunks))]
-    public void IsPrettyLine_Should_Work(string input, IEnumerable<int> expectedArr, bool expectedStatus, int expectedChanges)
+    public void IsPrettyLine_Should_Work(string input, IEnumerable<(int, char)> expectedArr, bool expectedStatus, int expectedChanges)
     {
         var chunks = PrettyStringParser.CountEqualChunks(input).ToList();
         chunks.Should().BeEquivalentTo(expectedArr);
@@ -19,18 +19,18 @@ public class Tests
     public static IEnumerable<TestCaseData> TestCasesForLineChunks()
     {
         // Pretty lines
-        yield return new TestCaseData("010101", new [] {1,1,1,1,1,1}, true, 0);
-        yield return new TestCaseData("000111", new [] {3, 3}, true, 0);
-        yield return new TestCaseData("111000", new [] {3, 3}, true, 0);
-        yield return new TestCaseData("1", new [] {1}, true, 0);
-        yield return new TestCaseData("110011", new [] {2, 2, 2}, true, 0);
-        yield return new TestCaseData("00000000", new [] {8}, true, 0);
-        // Ugly lines
-        yield return new TestCaseData("000101", new [] {3, 1, 1, 1}, false, 1);
-        yield return new TestCaseData("00110100", new [] {2, 2, 1, 1, 2}, false, 3);
-        yield return new TestCaseData("010111", new [] {1, 1, 1, 3}, false, 1);
-        yield return new TestCaseData("110111", new [] {2, 1, 3}, false, 1);
-        yield return new TestCaseData("110110", new [] {2, 1, 2, 1}, false, 2);
-        yield return new TestCaseData("111000", new [] {3, 3}, true, 0);
+        yield return new TestCaseData("010101", new [] {(1, '0'), (1, '1'),(1, '0'),(1, '1'),(1, '0'),(1, '1')}, true, 0);
+        yield return new TestCaseData("000111", new [] {(3, '0'), (3, '1')}, true, 0);
+        yield return new TestCaseData("111000", new [] {(3, '1'), (3, '0')}, true, 0);
+        yield return new TestCaseData("1", new [] {(1, '1')}, true, 0);
+        yield return new TestCaseData("110011", new [] {(2, '1'), (2, '0'), (2, '1')}, true, 0);
+        yield return new TestCaseData("00000000", new [] {(8, '0')}, true, 0);
+        // // Ugly lines
+        yield return new TestCaseData("000101", new [] {(3, '0'), (1, '1'), (1, '0'), (1, '1')}, false, 1);
+        yield return new TestCaseData("00110100", new [] {(2, '0'), (2, '1'), (1, '0'), (1, '1'), (2, '0')}, false, 3);
+        yield return new TestCaseData("010111", new [] {(1, '0'), (1, '1'), (1, '0'), (3, '1')}, false, 1);
+        yield return new TestCaseData("110111", new [] {(2, '1'), (1, '0'), (3, '1')}, false, 1);
+        yield return new TestCaseData("110110", new [] {(2, '1'), (1, '0'), (2, '1'), (1, '0')}, false, 2);
+        yield return new TestCaseData("0010001011111111", new [] {(2, '0'), (1, '1'), (3, '0'), (1, '1'), (1, '0'), (8, '1')}, false, 2);
     }
 }
